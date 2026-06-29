315abb0e7fac8905576f7d5298a0395d63a8d080
komaweb <marimoxinzhong@gmail.com>
2026-06-29 21:05:54 +0900

    Update tabs.js

--- js/components/tabs.js
+++ js/components/tabs.js
@@ -1,1 +1,86 @@
-export {};
+//======================================
+// Original Gacha Maker
+// components/tabs.js
+//======================================
+
+export function initTabs(){
+
+    const buttons =
+
+        document.querySelectorAll(
+
+            ".tab-button"
+
+        );
+
+    const pages =
+
+        document.querySelectorAll(
+
+            ".page"
+
+        );
+
+    buttons.forEach(button=>{
+
+        button.addEventListener(
+
+            "click",
+
+            ()=>{
+
+                buttons.forEach(
+
+                    button=>{
+
+                        button.classList.remove(
+
+                            "active"
+
+                        );
+
+                    }
+
+                );
+
+                pages.forEach(
+
+                    page=>{
+
+                        page.classList.remove(
+
+                            "active"
+
+                        );
+
+                    }
+
+                );
+
+                button.classList.add(
+
+                    "active"
+
+                );
+
+                document
+
+                    .getElementById(
+
+                        button.dataset.page
+
+                    )
+
+                    .classList.add(
+
+                        "active"
+
+                    );
+
+            }
+
+        );
+
+    });
+
+}
