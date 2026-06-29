68c4cd3829d5b7becc65a315103f73019776b8b7
komaweb <marimoxinzhong@gmail.com>
2026-06-29 21:10:59 +0900

    Update header.js

--- js/components/header.js
+++ js/components/header.js
@@ -1,1 +1,22 @@
-export {};
+//======================================
+// Original Gacha Maker
+// components/header.js
+//======================================
+
+export function createHeader(){
+
+    return `
+
+<header class="header">
+
+    <h1>
+
+        Original Gacha Maker
+
+    </h1>
+
+</header>
+
+`;
+
+}
