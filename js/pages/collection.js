a3de6b8969b5795b875a49c9d9bd24a9ca2b9e7a
komaweb <marimoxinzhong@gmail.com>
2026-06-29 21:34:36 +0900

    Update collection.js

--- js/pages/collection.js
+++ js/pages/collection.js
@@ -1,1 +1,56 @@
-export {};
+//======================================
+// Original Gacha Maker
+// pages/collection.js
+//======================================
+
+import {
+
+    createElement
+
+} from "../utils/dom.js";
+
+export function renderCollection(){
+
+    const page =
+
+        createElement(
+
+            "section",
+
+            "page"
+
+        );
+
+    page.id =
+
+        "collection";
+
+    const panel =
+
+        createElement(
+
+            "div",
+
+            "panel"
+
+        );
+
+    panel.innerHTML = `
+
+        <h2>
+
+            図鑑
+
+        </h2>
+
+    `;
+
+    page.appendChild(
+
+        panel
+
+    );
+
+    return page;
+
+}
