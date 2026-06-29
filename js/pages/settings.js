4064356708bcee5d290143bf8b3dbabbfba8ac63
komaweb <marimoxinzhong@gmail.com>
2026-06-29 21:34:49 +0900

    Update settings.js

--- js/pages/settings.js
+++ js/pages/settings.js
@@ -1,1 +1,56 @@
-export {};
+//======================================
+// Original Gacha Maker
+// pages/settings.js
+//======================================
+
+import {
+
+    createElement
+
+} from "../utils/dom.js";
+
+export function renderSettings(){
+
+    const page =
+
+        createElement(
+
+            "section",
+
+            "page"
+
+        );
+
+    page.id =
+
+        "settings";
+
+    const panel =
+
+        createElement(
+
+            "div",
+
+            "panel"
+
+        );
+
+    panel.innerHTML = `
+
+        <h2>
+
+            設定
+
+        </h2>
+
+    `;
+
+    page.appendChild(
+
+        panel
+
+    );
+
+    return page;
+
+}
