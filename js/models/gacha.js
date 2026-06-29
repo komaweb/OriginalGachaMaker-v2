c23feb9ec2351aeb67933c5a22ce6d6cd741beb9
komaweb <marimoxinzhong@gmail.com>
2026-06-29 21:58:15 +0900

    Update gacha.js

--- js/models/gacha.js
+++ js/models/gacha.js
@@ -1,1 +1,20 @@
-export {};
+//======================================
+// Original Gacha Maker
+// models/gacha.js
+//======================================
+
+export function createGacha(){
+
+    return{
+
+        id:crypto.randomUUID(),
+
+        name:"",
+
+        banner:null,
+
+        createdAt:Date.now()
+
+    };
+
+}
