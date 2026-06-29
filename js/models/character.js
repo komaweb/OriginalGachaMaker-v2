fe722404c9aae24100b129b0be6f1db0bc046b17
komaweb <marimoxinzhong@gmail.com>
2026-06-29 21:58:32 +0900

    Update character.js

--- js/models/character.js
+++ js/models/character.js
@@ -1,1 +1,32 @@
-export {};
+//======================================
+// Original Gacha Maker
+// models/character.js
+//======================================
+
+export function createCharacter(){
+
+    return{
+
+        id:crypto.randomUUID(),
+
+        gachaId:"",
+
+        name:"",
+
+        rarity:1,
+
+        image:null,
+
+        detailImage:null,
+
+        quote:"",
+
+        description:"",
+
+        obtained:false,
+
+        createdAt:Date.now()
+
+    };
+
+}
