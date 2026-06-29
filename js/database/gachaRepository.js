b67bb828b619a33fe7628c6334ca7b106a998828
komaweb <marimoxinzhong@gmail.com>
2026-06-29 20:59:47 +0900

    Update gachaRepository.js

--- js/database/gachaRepository.js
+++ js/database/gachaRepository.js
@@ -1,1 +1,208 @@
-export {};
+//======================================
+// Original Gacha Maker
+// database/gachaRepository.js
+//======================================
+
+import {
+
+    getDatabase,
+
+    STORES
+
+} from "./database.js";
+
+export async function getGachas(){
+
+    return new Promise(
+
+        (resolve,reject)=>{
+
+            const request =
+
+                getDatabase()
+
+                .transaction(
+
+                    STORES.gachas,
+
+                    "readonly"
+
+                )
+
+                .objectStore(
+
+                    STORES.gachas
+
+                )
+
+                .getAll();
+
+            request.onsuccess = ()=>{
+
+                resolve(
+
+                    request.result
+
+                );
+
+            };
+
+            request.onerror = ()=>{
+
+                reject(
+
+                    request.error
+
+                );
+
+            };
+
+        }
+
+    );
+
+}
+
+export async function addGacha(gacha){
+
+    return new Promise(
+
+        (resolve,reject)=>{
+
+            const request =
+
+                getDatabase()
+
+                .transaction(
+
+                    STORES.gachas,
+
+                    "readwrite"
+
+                )
+
+                .objectStore(
+
+                    STORES.gachas
+
+                )
+
+                .add(gacha);
+
+            request.onsuccess = ()=>{
+
+                resolve();
+
+            };
+
+            request.onerror = ()=>{
+
+                reject(
+
+                    request.error
+
+                );
+
+            };
+
+        }
+
+    );
+
+}
+
+export async function updateGacha(gacha){
+
+    return new Promise(
+
+        (resolve,reject)=>{
+
+            const request =
+
+                getDatabase()
+
+                .transaction(
+
+                    STORES.gachas,
+
+                    "readwrite"
+
+                )
+
+                .objectStore(
+
+                    STORES.gachas
+
+                )
+
+                .put(gacha);
+
+            request.onsuccess = ()=>{
+
+                resolve();
+
+            };
+
+            request.onerror = ()=>{
+
+                reject(
+
+                    request.error
+
+                );
+
+            };
+
+        }
+
+    );
+
+}
+
+export async function deleteGacha(id){
+
+    return new Promise(
+
+        (resolve,reject)=>{
+
+            const request =
+
+                getDatabase()
+
+                .transaction(
+
+                    STORES.gachas,
+
+                    "readwrite"
+
+                )
+
+                .objectStore(
+
+                    STORES.gachas
+
+                )
+
+                .delete(id);
+
+            request.onsuccess = ()=>{
+
+                resolve();
+
+            };
+
+            request.onerror = ()=>{
+
+                reject(
+
+                    request.error
+
+                );
+
+            };
+
+        }
+
+    );
+
+}
