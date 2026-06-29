8dc6bba3305ead5493824bc7f8532ab96d4eff3b
komaweb <marimoxinzhong@gmail.com>
2026-06-29 21:33:38 +0900

    Update gacha.js

--- js/pages/gacha.js
+++ js/pages/gacha.js
@@ -1,1 +1,39 @@
-export {};
+import {
+
+    createElement
+
+} from "../utils/dom.js";
+
+export function renderGacha(){
+
+    const page =
+
+        createElement(
+
+            "section",
+
+            "page"
+
+        );
+
+    page.id =
+
+        "gacha";
+
+    page.innerHTML = `
+
+        <div class="panel">
+
+            <h2>
+
+                ガチャ
+
+            </h2>
+
+        </div>
+
+    `;
+
+    return page;
+
+}
