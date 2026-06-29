e64a0e62abb74e4256a84198203fe930b29a578d
komaweb <marimoxinzhong@gmail.com>
2026-06-29 21:06:41 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,50 @@
-export {};
+//======================================
+// Original Gacha Maker
+// app.js
+//======================================
+
+import {
+
+    initDatabase
+
+} from "./database/database.js";
+
+import {
+
+    initTabs
+
+} from "./components/tabs.js";
+
+window.addEventListener(
+
+    "DOMContentLoaded",
+
+    async()=>{
+
+        try{
+
+            await initDatabase();
+
+            initTabs();
+
+            console.log(
+
+                "Original Gacha Maker v2"
+
+            );
+
+        }catch(error){
+
+            console.error(error);
+
+            alert(
+
+                "初期化に失敗しました。"
+
+            );
+
+        }
+
+    }
+
+);
