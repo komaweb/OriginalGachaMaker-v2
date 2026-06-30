d082fa52aa694c2b25595ff0401a20ff9706025e
komaweb <marimoxinzhong@gmail.com>
2026-06-30 11:57:04 +0900

    Update gachaAnimation.js

--- js/animation/gachaAnimation.js
+++ js/animation/gachaAnimation.js
@@ -73,35 +73,7 @@ export async function playGachaAnimation(
 
         );
 
-    const grid =
 
-    document.getElementById(
-
-        "presentGrid"
-
-    );
-
-if(
-
-    results.length===1
-
-){
-
-    grid.classList.add(
-
-        "single"
-
-    );
-
-}else{
-
-    grid.classList.remove(
-
-        "single"
-
-    );
-
-}
 
     overlay.classList.remove(
 
